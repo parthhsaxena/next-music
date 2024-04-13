469828494343650e4806dac438aa1109e9647a9b
Parth Saxena <parthsxn7@gmail.com>
2024-04-13 14:27:35 +0530

    added footer and courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+    <main className="min-h-screen bg-black/[0.96] antialiased ">
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
       <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
